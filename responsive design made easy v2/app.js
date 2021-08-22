4a403c30ef7438b017dd3480c485d448f23cb832
Mads-SJ <75970641+Mads-SJ@users.noreply.github.com>
2021-08-22 11:24:43 +0200

    Delete responsive design made easy v2 directory

--- responsive design made easy v2/app.js
+++
@@ -1,7 +0,0 @@
-const navBtn = document.querySelector(".nav-btn");
-const navLinks = document.querySelector(".nav-links");
-
-navBtn.addEventListener("click", () => {
-    navLinks.classList.toggle("dropdown");
-    navBtn.classList.toggle("pressed");
-});

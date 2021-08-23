8d4d3182cabbfb837fd683f07cdd707c2ed64b6a
Mads <mads.soenderstrup@gmail.com>
2021-08-23 10:57:01 +0200

    added hover on nav-links and commented in app.js

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ let navLinks = document.querySelectorAll(".nav-links li, .logo");
 
 
 function navSlide() {
+    // Toggle dropdown-menu and navigation button
     navBtn.addEventListener("click", () => {
         dropdownMenu.classList.toggle("dropdown");
         navBtn.classList.toggle("pressed");
     });
     
+    // Close dropdown-menu when clicking on links, including the logo
     navLinks.forEach(link => {
         link.addEventListener("click", () => {
             dropdownMenu.classList.remove("dropdown");
